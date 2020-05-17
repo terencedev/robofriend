18f93ee85c440d6b7bc78e4b763b482314e6092a
Terence <terence.development@gmail.com>
2020-05-17 12:59:04 +0800

    Updates

---
+++ precache-manifest.4223db293894d15e7a0d98699c994cbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc07dcd9ea7232b65b4a3d234c00c043",
+    "url": "/terencedev/robofriend/index.html"
+  },
+  {
+    "revision": "c83e07f37480374bdc52",
+    "url": "/terencedev/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d4b617db6dc871136e94",
+    "url": "/terencedev/robofriend/static/css/main.13603936.chunk.css"
+  },
+  {
+    "revision": "c83e07f37480374bdc52",
+    "url": "/terencedev/robofriend/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/terencedev/robofriend/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b617db6dc871136e94",
+    "url": "/terencedev/robofriend/static/js/main.ae9fe6a6.chunk.js"
+  },
+  {
+    "revision": "c1b86caac74a96b482f2",
+    "url": "/terencedev/robofriend/static/js/runtime-main.1ebaa3bd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/terencedev/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
